d8b9ce447a48d2b7ea8ac9071dd2f24a404b2709
aliharrath <ali.harrath@esprit.tn>
2019-04-22 13:40:49 +0100

    ali update

---
+++ integration/views/wishlist2.php
@@ -0,0 +1,65 @@
+<?php session_start(); 
+include "../core/categorieC.php";
+include "../core/produitC.php";
+include "../core/wishlistC.php";
+$categorieC =new categorieC();
+$listeCategorieC=$categorieC->afficherCategorie();   
+$produitC =new ProduitC();
+$listeProduit=$produitC->afficherProduit(); 
+$nbr=$produitC->Number();
+$wishC =new wishlistC();
+$wosh=$wishC->Viewwishlist((int)$_SESSION['user_id']); 
+var_dump($wosh);
+?>
+	<div class="content">
+            <div class="animated fadeIn">
+                <div class="row">
+                    <div class="col-lg-12">
+                        <div class="card">
+                            <div class="card-header">
+                                <strong class="card-title">Unfilled Orders</strong>
+                            </div>
+                            <div class="table-stats order-table ov-h">
+                                <table class="table ">
+                                    <thead>
+                                        <tr>
+                                            <th>Name</th>
+                                            <th>Surname</th>
+                                            <th>Email</th>
+                                           
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                    <?php if(!empty($wosh)){ 
+                                    foreach($wosh as $w){ ?>
+		                            <tr>
+                                    <td><?php echo $w['nom']; ?></td>
+                                    <td><?php echo $w['prix']; ?></td>
+                                    <td><?php echo $w['quantite']; ?></td>
+                                    </tr>
+	                                    <?php	}	}else{ ?><p>No products were added to the wishlist</p>
+	                                    <?php }?>
+                                        
+								
+									}}
+									?>
+                                    </tbody>
+                                </table>
+                            </div> <!-- /.table-stats -->
+                        </div>
+						
+						<a href="?Delete_All=1" ><button type="button">Delete All </button></a>
+						
+						
+                                    </tbody>
+                                </table>
+                            </div> <!-- /.table-stats -->
+                        </div>
+                    </div>
+        </div>
+    </div><!-- .animated -->
+</div>
+
+<?php
+	require 'Footer.php' ;
+?>
